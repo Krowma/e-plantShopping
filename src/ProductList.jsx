b962a0855ed90d75d195af6eac289a8b9782840b
Krowma <antoine.david.n7@gmail.com>
2026-05-26 09:56:28 -0400

    Display plant list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { addItem, removeItem } from './CartSlice';
 import './ProductList.css'
 import CartItem from './CartItem';
 function ProductList({ onHomeClick }) {
@@ -233,6 +235,13 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    const {addedToCart, setAddedToCart} = useState({});
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevSate) => ({...prevSate, [product.name]: true});
+    };
+
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -274,7 +283,24 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1> <div>{category.category}</div> </h1>
+                            <div className="product-list">
+                                {category.plants.map((plant, plantIndex) =>(
+                                    <div className="product-card" key={plantIndex}>
+                                        <img className="product-image" src={plant.image} alt={plant.name} />
+                                        <div className="product-title">{plant.name}</div>
+                                        <div className="product-description">{plant.description}</div>
+                                        <div className="product-cost">{plant.cost}</div>
+                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>
+                                            Add to Cart
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
 
                 </div>
             ) : (
